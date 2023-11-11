6608977b6615603b3910f43e3a93338c80e278c9
Alexey Korepanov <alexkorep@gmail.com>
2023-11-11 23:08:41 +0100

    Add game level resource

---
+++ assets/levels/level_structure.gd
@@ -0,0 +1,6 @@
+extends Resource
+class_name LevelStructure
+
+export(String) var text = ""
+export(Texture) var image
+
--- scenes/room.gd
+++ scenes/room.gd
@@ -1,9 +1,16 @@
 extends Sprite
 
+var current_level = 0
+var levels = []
+var task_description
+
 onready var progress_bar = get_parent().get_node("HealthProgressBar")
 
 func _ready():
+	task_description = $"../TaskDescription"
 	progress_bar.value = 0
+	levels = load_levels()
+	update_display()
 
 func _input(event):
 	# Check if the event is a mouse button event, 
@@ -12,7 +19,38 @@ func _input(event):
 		and event.pressed 
 		and self.get_rect().has_point(to_local(event.position))):
 		progress_bar.value += 10  # Increase by 10 each click, or your desired increment.
+		if progress_bar.value >= 100:
+			progress_bar.value = 0
+			current_level += 1
+			if current_level >= len(levels):
+				current_level = 0
+			update_display()
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func update_display():
+	var level = levels[current_level]
+	self.texture = level.image
+	task_description.text = level.text
+	
+
+func load_levels():
+	var PATH = "res://assets/levels"
+	var resources_array = []
+	var dir = Directory.new()
+	# Open the directory
+	if dir.open(PATH) == OK:
+		# List all files in the directory
+		dir.list_dir_begin()
+		var file_name = dir.get_next()
+		while file_name != "":
+			if file_name.ends_with(".tres"):  # Check for .tres files
+				# Load the resource and append it to the array
+				var res = load(PATH + "/" + file_name)
+				if res is LevelStructure:
+					resources_array.append(res)
+			file_name = dir.get_next()
+		dir.list_dir_end()
+	else:
+		print("An error occurred when trying to open the directory.")
+	print(resources_array)
+	return resources_array
+
